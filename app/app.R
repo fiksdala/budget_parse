6f0e616996bbdb08bc3d40ab49d169bd40853f93
Alexander Fiksdal <alexander.fiksdal@gmail.com>
2022-01-15 10:03:59 -0500

    Adds checking/pending tables, UI improvements

--- app/app.R
+++ app/app.R
@@ -8,26 +8,100 @@ source('functions.R')
 
 # The UI
 ui <- bootstrapPage(
-    
-    textAreaInput('my_input', "Paste Capital One Output Here:"),
-    h3('Press "copy" below for formatted output.'),
-    h6('"Missing value" error is normal before you enter text above.'),
-    DTOutput('output.table')
+    h3('Input data below:'),
+    textAreaInput('posted.input', "Paste Capital One 'Posted' Output Here:"),
+    textAreaInput('pending.input', "Paste Capital One 'Pending' Output Here:"),
+    fileInput(
+        'checking', 
+        'Upload checking .csv here',
+        accept=c(
+            "text/csv",
+            "text/comma-separated-values,text/plain",
+            ".csv"
+        )
+    ),
+    h3('Press "copy" for formatted output.'),
+    h4('Posted Output'),
+    DTOutput('posted.table'),
+    h4('Pending Output'),
+    DTOutput('pending.table'),
+    h4('Checking Output'),
+    DTOutput('output.checking')
     
 )
 
 # The server
 server <- function(input, output) {
     
-    # Make the table
-    formatted.table <- reactive({
-        format_capitalone(input$my_input)
+    # Make the tables
+    formatted.table.posted <- reactive({
+        format_capitalone(input$posted.input)
     })
     
-    output$output.table <- renderDT(server=FALSE,{
+    formatted.table.pending <- reactive({
+        format_capitalone(input$pending.input)
+    })
+    
+    output$posted.table <- renderDT(server=FALSE,{
 
         # Show data
-        datatable(formatted.table(), 
+        datatable(formatted.table.posted(), 
+                  extensions = 'Buttons', 
+                  options = list(dom = 'tB',
+                                 style='bootstrap',
+                                 buttons = list(
+                                     list(
+                                         extend = 'copy', 
+                                         title = NULL
+                                     ) 
+                                 )),
+                  rownames = F)
+    })
+    
+    output$pending.table <- renderDT(server=FALSE,{
+        
+        # Show data
+        datatable(formatted.table.pending(), 
+                  extensions = 'Buttons', 
+                  options = list(dom = 'tB',
+                                 style='bootstrap',
+                                 buttons = list(
+                                     list(
+                                         extend = 'copy', 
+                                         title = NULL
+                                     ) 
+                                 )),
+                  rownames = F)
+    })
+    
+    # Make the checking csv table
+    output$output.checking <- renderDT(server=FALSE,{
+        
+        inFile <- input$checking
+        if (is.null(inFile)){
+            return(
+                datatable(
+                    data.frame(
+                        Upload='',
+                        File='',
+                        For='',
+                        Table=''
+                    ),
+                    extensions = 'Buttons', 
+                    options = list(dom = 'tB',
+                                   style='bootstrap',
+                                   buttons = list(
+                                       list(
+                                           extend = 'copy', 
+                                           title = NULL
+                                       ) 
+                                   )),
+                    rownames = F
+                )
+            )
+        } 
+        # Show data
+        datatable(read.csv(inFile$datapath), 
                   extensions = 'Buttons', 
                   options = list(dom = 'tB',
                                  style='bootstrap',
